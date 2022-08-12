a7ee3dafd9daafb4abe6b9d4b56e296bfd57dc21
svvalentyna <svvalentyna@gmail.com>
2022-08-12 15:21:56 +0300

    feat: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 
+  let openBrackets = [];
+  let bracketsPair = {}
+  let bracketsSamePair = [];
+  for(let i = 0; i < bracketsConfig.length; i++){
+    openBrackets.push(bracketsConfig[i][0])
+    if(bracketsConfig[i][1] === bracketsConfig[i][0]){
+      bracketsSamePair.push(bracketsConfig[i][1])
+    }
+    bracketsPair[bracketsConfig[i][1]] = bracketsConfig[i][0]
+  }
+  
+  let stack = [];
+  for(let i=0; i < str.length; i++){
+    let currentSymbol = str[i];
+    if(openBrackets.includes(currentSymbol)){
+      if(bracketsSamePair.includes(currentSymbol)){
+        if(stack.includes(currentSymbol)){
+          let topEl = stack[stack.length - 1];
+          if(bracketsPair[currentSymbol] === topEl){
+            stack.pop();
+          } else {
+            return false;
+          }
+        } else {
+
+          stack.push(currentSymbol);
+        }
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else {
+      if(stack.length === 0){
+        return false;
+      }
+      let topEl = stack[stack.length - 1];
+      if(bracketsPair[currentSymbol] === topEl){
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  } 
+  
+    return stack.length === 0
 }
